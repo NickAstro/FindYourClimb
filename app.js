4f122f5da166e3849a3e793b012817cc6e0e8477
Louie Nicastro <louis.m.nicastro@gmail.com>
2018-04-19 19:41:13 +0000

    added submit new location page , bootstrap, partials

--- app.js
+++ app.js
@@ -1,21 +1,37 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
- 
+// initial array of objects - to be converted to DB later
+var locations = [
+    {name: "Delaware Rock Gym", image: "http://www.derockgym.com/images/boulder2.jpg"},
+    {name: "Philly Rock Gym", image: "https://www.philarockgym.com/wp-content/uploads/Iron_Oaks_Studio_Photos/IOS_prg-eastfalls_09-1024x683.jpg"}
+]
 
 app.get("/", function(req, res) {
    res.render("landing"); 
 });
 
 app.get("/locations", function(req, res) {
-    // initial array of objects - to be converted to DB later
-    var locations = [
-        {name: "Delaware Rock Gym", image: "http://www.derockgym.com/images/boulder2.jpg"},
-        {name: "Philly Rock Gym", image: "https://www.philarockgym.com/wp-content/uploads/Iron_Oaks_Studio_Photos/IOS_prg-eastfalls_09-1024x683.jpg"}
-    ]
+
    res.render("locations", {locations:locations}); 
+});
+
+app.post("/locations", function(req, res) {
+    var name = req.body.name;
+    var image = req.body.image;
+    var newSite = {name: name, image: image};
+    locations.push(newSite);
+    res.redirect("/locations");
+    
+});
+
+app.get("/locations/new", function(req, res) {
+   res.render("new"); 
+   
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
