dd1986c8663c5afa325ff3c8cb77422f5885b0dc
Louie Nicastro <louis.m.nicastro@gmail.com>
2018-04-23 13:06:42 +0000

    add thumbnails and mild styling to locations page (jumbotron)

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ app.set("view engine", "ejs");
 // initial array of objects - to be converted to DB later
 var locations = [
     {name: "Delaware Rock Gym", image: "http://www.derockgym.com/images/boulder2.jpg"},
-    {name: "Philly Rock Gym", image: "https://www.philarockgym.com/wp-content/uploads/Iron_Oaks_Studio_Photos/IOS_prg-eastfalls_09-1024x683.jpg"}
+    {name: "Philly Rock Gym", image: "https://www.philarockgym.com/wp-content/uploads/Iron_Oaks_Studio_Photos/IOS_prg-eastfalls_09-1024x683.jpg"},
+    {name: "Delaware Rock Gym", image: "http://www.derockgym.com/images/boulder2.jpg"},
+    {name: "Philly Rock Gym", image: "https://www.philarockgym.com/wp-content/uploads/Iron_Oaks_Studio_Photos/IOS_prg-eastfalls_09-1024x683.jpg"},
+    {name: "Delaware Rock Gym", image: "http://www.derockgym.com/images/boulder2.jpg"},
+    {name: "Philly Rock Gym", image: "https://www.philarockgym.com/wp-content/uploads/Iron_Oaks_Studio_Photos/IOS_prg-eastfalls_09-1024x683.jpg"},
+    
 ]
 
 app.get("/", function(req, res) {
